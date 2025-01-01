ec0d71d6e3ef2fa0f42df285f1ec11daf5a468d0
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-01-01 18:44:38 +0100

    updating

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,32 +1,48 @@
-import {Link} from "react-router";
-import {motion} from "framer-motion";
+import { Link } from "react-router";
+import { motion } from "framer-motion";
 
 const Login = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
-    return (<div className='w-full h-[100vh] flex flex-col items-center justify-center'>
-            <form className=' w-1/3' onSubmit={handleSubmit}>
-                <h1 className='text-3xl text-center font-semibold'>Welcome Back !</h1>
-                <p className='text-sm text-slate-500 text-center  mb-5'>Please enter your details</p>
-                <div className='flex flex-col'>
-                    <label htmlFor="email">Email</label>
-                    <input type='email' className='outline-none   border-b-2'/>
-                </div>
-                <div className='flex flex-col mt-4'>
-                    <label htmlFor="password">Password</label>
-                    <input type='password' className='outline-none   border-b-2'/>
-                </div>
-                <p className='flex justify-end mt-4 text-slate-500'><Link to='/register'>Forget Password ?</Link></p>
-                <motion.button
-                    whileTap={{
-                        scale: 0.95,
-                    }}
-
-                    className='relative hover:text-white overflow-hidden hoveredBtn w-full mt-3 border  border-black py-2 rounded'>Login
-                </motion.button>
-            </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+      <form
+        className="w-full px-10 sm:w-[80%] md:w-[70%] lg:w-1/3"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-3xl text-center font-semibold">Welcome Back !</h1>
+        <p className="text-sm text-slate-500 text-center  mb-5">
+          Please enter your details
+        </p>
+        <div className="flex flex-col">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="outline-none   border-b-2" />
         </div>
-    )
-}
+        <div className="flex flex-col mt-4">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="outline-none   border-b-2" />
+        </div>
+        <p className="flex justify-end mt-4 text-slate-500">
+          <Link to="/register">Forget Password ?</Link>
+        </p>
+        <motion.button
+          whileTap={{
+            scale: 0.98,
+          }}
+          whileHover={{
+            scale : 1.01
+          }}
+          transition={{
+            duration : 0.1,
+            ease : 'easeIn'
+          }}
+          className="relative hover:text-[#4F46E5]  overflow-hidden hover:bg-[#EEF2FF] w-full mt-3 border  border-black py-2 rounded-full"
+        >
+          Login
+        </motion.button>
+      </form>
+    </div>
+  );
+};
 export default Login;
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,51 +1,110 @@
-import {Link} from "react-router";
-import {motion} from "framer-motion";
-import {LiaEyeSolid} from "react-icons/lia";
-import {useState} from "react";
-import {FaRegEyeSlash} from "react-icons/fa";
-
+import { Link } from "react-router";
+import { motion } from "framer-motion";
+import { LiaEyeSolid } from "react-icons/lia";
+import { useState } from "react";
+import { FaRegEyeSlash } from "react-icons/fa";
+import Alert from "./required-componants/Alert.jsx";
+import axiosInstance from "./required-componants/axios-Instance.jsx";
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
-    const HandlePassword = () => {
-        setShowPassword(!showPassword);
-    }
-    return (<div className='w-full h-[100vh] flex flex-col items-center justify-center'>
-            <form className=' w-1/3' onSubmit={handleSubmit}>
-                <h1 className='text-3xl text-center font-semibold'>Welcome Sir !</h1>
-                <p className='text-sm text-slate-500 text-center  mb-5'>Please The required details</p>
-                <div className='flex flex-col'>
-                    <label htmlFor="name">Name</label>
-                    <input type='text' className='outline-none   border-b-2'/>
-                </div>
-                <div className='flex flex-col mt-4'>
-                    <label htmlFor="email">Email</label>
-                    <input type='email' className='outline-none   border-b-2'/>
-                </div>
-                <div className='flex flex-col mt-4 relative'>
-                    <label htmlFor="password">Password</label>
-                    <input type='password' className='outline-none   border-b-2'/>
-                    {showPassword ?
-                        <LiaEyeSolid onClick={HandlePassword} className='absolute right-0  top-1/2 cursor-pointer'/> :
-                        <FaRegEyeSlash onClick={HandlePassword} className='absolute right-0  top-1/2 cursor-pointer'/>}
-                </div>
-                <div className='flex flex-col mt-4 relative'>
-                    <label htmlFor="password">Confirm Password</label>
-                    <input type='password' className='outline-none   border-b-2'/>
-                </div>
-                <p className='flex justify-end mt-4 text-slate-500'><Link to='/register'>Forget Password ?</Link></p>
-                <motion.button
-                    whileTap={{
-                        scale: 0.95,
-                    }}
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-                    className='relative hover:text-white overflow-hidden hoveredBtn w-full mt-3 border  border-black py-2 rounded'>Register
-                </motion.button>
-            </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password || !name || !confirmPassword) {
+      setError("fields are required!");
+      setTimeout(() => {
+        setError(false);
+      }, 1500);
+    } else {
+      axiosInstance
+        .post("/Register", {
+          name: name,
+          email: email,
+          password: password,
+          password_confirmation: confirmPassword,
+        })
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const HandlePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+      {error && <Alert type={"error"} content={error} />}
+      <form
+        className=" w-full px-10 sm:w-[80%] md:w-[70%] lg:w-1/3 "
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-3xl text-center font-semibold">Welcome Sir !</h1>
+        <p className="text-sm text-slate-500 text-center  mb-5">
+          Please The required details
+        </p>
+        <div className="flex flex-col">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="outline-none   border-b-2"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    )
-}
+        <div className="flex flex-col mt-1">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="outline-none   border-b-2"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col mt-1 relative">
+          <label htmlFor="password">Password</label>
+          <input
+            type={showPassword ? "text" : "password"}
+            className="outline-none   border-b-2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {showPassword ? (
+            <LiaEyeSolid
+              onClick={HandlePassword}
+              className="absolute right-0  top-1/2 cursor-pointer"
+            />
+          ) : (
+            <FaRegEyeSlash
+              onClick={HandlePassword}
+              className="absolute right-0  top-1/2 cursor-pointer"
+            />
+          )}
+        </div>
+        <div className="flex flex-col mt-1 relative">
+          <label htmlFor="password">Confirm Password</label>
+          <input
+            type={showPassword ? "text" : "password"}
+            className="outline-none   border-b-2"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <p className="flex justify-end mt-1 text-slate-500">
+          <Link to="/register">Forget Password ?</Link>
+        </p>
+        <motion.button
+          whileTap={{
+            scale: 0.95,
+          }}
+          className="relative hover:text-[#4F46E5] overflow-hidden hover:bg-[#EEF2FF] w-full mt-3 border  border-black py-2 rounded-full"
+        >
+          Register
+        </motion.button>
+      </form>
+    </div>
+  );
+};
 export default Login;
